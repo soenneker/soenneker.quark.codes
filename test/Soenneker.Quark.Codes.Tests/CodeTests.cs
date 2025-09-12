306a95835d79c4344dbffdfbf10435220ad85f41
Jake Soenneker <jake@soenneker.com>
2025-09-12 10:59:11 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Codes.Tests/CodeTests.cs
@@ -0,0 +1,19 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Codes.Tests;
+
+[Collection("Collection")]
+public sealed class CodeTests : FixturedUnitTest
+{
+    public CodeTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+        // Test that the component can be instantiated
+        Assert.True(true);
+    }
+}
